b62d6fb90b36f377445e2f6aadb19556d1b80dea
Ondrej Janus <ondrej.janus@konicaminolta.cz>
2018-06-29 13:19:48 +0200

    Add script for unsetting of alt + f7 elementary os shortcuts.

---
+++ set_shortcuts.sh
@@ -0,0 +1,6 @@
+gsettings set org.gnome.mutter.wayland.keybindings switch-to-session-7 "['']"
+gsettings set org.gnome.desktop.wm.keybindings begin-move "['']"
+
+# gsettings reset org.gnome.mutter.wayland.keybindings switch-to-session-7
+# gsettings reset org.gnome.desktop.wm.keybindings begin-move
+
