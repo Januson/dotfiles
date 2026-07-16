f6656ed2aeab8eb7aecf25528e4d4038ec435d0f
Ondrej Janus <janusonx@gmail.com>
2026-07-16 17:37:21 +0200

    Added script for font installation

---
+++ install_hack_font.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -e
+
+echo "Downloading..."
+curl -o /tmp/Hack.zip -L https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/Hack.zip
+
+echo "Unzipping..."
+unzip /tmp/Hack.zip -d ~/.local/share/fonts/
+
+echo "Installing..."
+fc-cache -vf ~/.local/share/fonts/
+
+echo "Verifying..."
+fc-list : family style | grep -i nerd
